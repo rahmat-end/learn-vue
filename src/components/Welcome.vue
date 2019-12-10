d7dde612da4828e814ba4664bde1392d45c00c21
Rahmat Kurniawan <rahmatkurniawan689@gmail.com>
2019-12-10 14:04:40 +0700

    first commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="welcome">
+        <div class="header-wrap">
+            <h1>{{ msg }}</h1>
+            <p>{{ msg }}</p>
+            <ul class="header-list">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      msg: 'Welcome mas broo'
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  test: /\.scss$/,
+  use: [
+    'vue-style-loader',
+    'css-loader',
+    {
+      loader: 'sass-loader',
+      options: {
+        data: `
+          @import "@/stylesheets/main.scss";
+        `
+      }
+    }
+  ],
+  router,
+  components: { App },
+  template: '<App/>'
+})
